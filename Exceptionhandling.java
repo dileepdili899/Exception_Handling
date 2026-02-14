11ace35f330831c4c4318785349600294cb1eae8
dileepdili899 <dileepdili899@gmail.com>
2026-02-14 11:53:03 +0530

    Completed till Exception

---
+++ Exceptionhandling.java
@@ -0,0 +1,40 @@
+class Exception1{
+    public static void main(String[] args) {
+        int a=10;
+        int b=0;
+        try{
+            System.out.println(a/b);
+        }
+        catch(Exception e){
+            System.out.println("Cannot divided by zero");
+        }
+        System.out.println("Program is running successfully");
+    }
+}
+
+class Exception2{
+    public static void main(String[] args) {
+        String str=null;
+        try{
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("Null pointer exception"+ e.getMessage());
+        }
+        System.out.println("Program is running successfully");
+    }
+}
+
+
+class Exception3{
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        try{
+            System.out.println(arr[10]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index is oout of bounds"+ e.getMessage());
+        }
+        System.out.println("Program continues after handling the exception");
+    }
+}
